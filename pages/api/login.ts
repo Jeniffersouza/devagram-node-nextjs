d52da5dca2531d988c9ad202477543c38c3df805
Jeniffer souza <ajenifferbsouza@gmail.com>
2022-11-06 18:04:56 -0300

    encontrando usuario no banco

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,18 +2,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import md5 from "md5";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import type { UsuarioRequisicao } from "../../types/UsuarioRequisicao";
 
-const endpointLogin = (
+const endpointLogin = async (
   req : NextApiRequest,
   res : NextApiResponse<RespostaPadraoMsg>
 ) => {
+  
   if(req.method === 'POST'){
-    const {login, senha} = req.body;
-    
-    if (login === 'admin@admin.com' &&
-          senha === 'admin@123'){
-            return res.status(200).json({msg : 'USUARIO AUTENTICADO COM SUCESSO'})
-          }
+
+    const usuario = req.body as UsuarioRequisicao;
+
+    const usuariosEncontrados = await UsuarioModel.find({email : usuario.email, senha : md5(usuario.senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncotrado = usuariosEncontrados[0];
+            return res.status(200).json({msg : `Usuario ${usuarioEncotrado.nome} autenticado com sucesso`});
+        }
           return res.status(400).json({erro : 'Usuario ou senha não encontrados'})
   }
   return res.status(405).json({erro : 'Método informado não é válido!'})
